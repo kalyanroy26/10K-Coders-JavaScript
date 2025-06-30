58b3da3e0b2c2a2b8aa634afb204f9e883fdf800
kalyanroy26 <kalyanroy8143@gmail.com>
2025-06-30 18:23:15 +0530

    task added

---
+++ Day-24/script.js
@@ -0,0 +1,15 @@
+let data
+
+fetch('https://cataas.com/cat/gif')
+.then(response=>response.url)
+.then((data)=>{
+    let img = document.createElement('img')
+    img.setAttribute('src',data)
+    img.setAttribute('height',250)
+    document.body.append(img)
+    console.log(data);
+    
+})
+.catch((err)=>{
+    console.log("error:",err);
+})
